75fcbb5eb0d741024005971900708277d80da796
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-08-11 13:31:37 -0400

    finished 30 minutes into the first video building out the footer css is where I ended for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import Footer from './components/Footer/Footer'
+import Header from './components/Header/Header'
+import Homepage from './pages/Homepage'
 
 function App() {
 
   return (
     <div className='App'>
-      Hello React
+      <Header />
+      <Homepage />
+      <Footer />
     </div>
   )
 }
