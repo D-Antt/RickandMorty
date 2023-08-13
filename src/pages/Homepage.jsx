dd60b2c5cefcb2bfc4af7af3ba273db3d97ef235
Dan Anttila <115313515+D-Antt@users.noreply.github.com>
2023-08-13 16:11:31 -0400

    lesson 1 is finished

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import "./Homepage.css"
+import React, {useEffect, useState} from 'react'
+import './Homepage.css'
+import axios from 'axios'
+import CharacterCard from '../components/CharacterCard/CharacterCard'
 
 function Homepage() {
+
+    //create state to hold characters
+    const [characters, setCharacters] = useState([])
+
+    //this page should show all the characters when it loads
+    //create useEffect for this
+    //https://rickandmortyapi.com/api/character
+
+    useEffect(
+        ()=>{
+            //console.log('homepage loaded')
+            //make api call to get characters
+            axios.get(`https://rickandmortyapi.com/api/character`)
+            .then(res =>{
+                console.log(res.data.results)
+                //I have the character data, where do I store it?
+                //store it in state
+                setCharacters(res.data.results)
+
+            })
+            .catch(err => console.log(err))
+
+        }, [] //means it runs only once when the page loads
+    )
+
   return (
-    <div>Homepage</div>
+    <div className="home-container">
+        
+        <h1>Main Characters</h1>
+        <div className="characters-container">
+            {
+              characters.map(item => <CharacterCard key={item.id} character={item} />)
+
+            }
+        </div>
+    </div>
   )
 }
 
